8e870107bead1159a6acf84785d5adfe251ea5ff
Dominik Tobschall <dominik@fruux.com>
2014-08-08 11:31:40 +0200

    moved to example directory

--- example/server.php
+++ example/server.php
@@ -4,9 +4,6 @@ use \Dominik\Stellar\Federation\Server;
 
 include __DIR__ . '/../vendor/autoload.php';
 
-/*
- * @TODO Configuration needs to get moved elsewhere.
- */
 $resolver = [
     'example.org' => [
         'user' => 'gDnu3fdGNNAuUy84DmbfyxwELjfu8kpmHg',
